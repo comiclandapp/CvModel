8ba40d6ecba1178e28a20a234571df31f9c362dd
Antonio Montes <comicland_app@outlook.com>
2024-09-25 10:14:57 -0500

    remove keys

--- Sources/CvModel/CvConstants.swift
+++ Sources/CvModel/CvConstants.swift
@@ -18,6 +18,3 @@ public struct CvConstants {
     public static let cvBaseUrl_ = "https://comicvine.gamespot.com"
     public static let cvBaseUrl = URL(string: "https://comicvine.gamespot.com/api")!
 }
-//static const NSString* comicvineKey     = @"7bc11ea25b7189cde1c076ab4e3adb76db46f8a9"; // my key
-//static const NSString* comicvineKey     = @"f363cf3bc6d6a385f9df0353fb3251f4ef8b0fc6"; // somebody else's key
-//static const NSString* comicvineKey     = @"1db1b79d579dd5ec4cc77759dc074c4348aa3b6f"; // somebody else's key
